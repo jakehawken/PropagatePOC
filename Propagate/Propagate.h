ff68f84ea16f3004bda5a1128ab044bb2942a738
Jake Hawken <jacobdhawken@gmail.com>
2020-04-05 16:52:19 -0700

    Initial Commit

---
+++ Propagate/Propagate.h
@@ -0,0 +1,19 @@
+//
+//  Propagate.h
+//  Propagate
+//
+//  Created by Jake Hawken on 4/5/20.
+//  Copyright © 2020 Jake Hawken. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Propagate.
+FOUNDATION_EXPORT double PropagateVersionNumber;
+
+//! Project version string for Propagate.
+FOUNDATION_EXPORT const unsigned char PropagateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Propagate/PublicHeader.h>
+
+
